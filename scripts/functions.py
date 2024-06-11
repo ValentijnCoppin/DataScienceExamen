99c9204a1c763aaef07215daf2eea93ae4d10c25
ignition <valentijn.ignition@gmail.com>
2024-06-11 17:45:44 +0200

    Updated after exam to be ready for part 2

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,11 +1,12 @@
-import pandas as pd
 import os
-import json
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as pl
 
 
-def read_csv_files(directory_path):
+def read_csv_files(directory_path) -> pd.DataFrame:
     """
-    Function to read JSON files and create DataFrame
+    Function to read csv files and create DataFrame
 
     Args:
         directory_path (str): The path to the folder containing csv files.
@@ -15,11 +16,40 @@ def read_csv_files(directory_path):
     """
     dataframes = []
     for filename in os.listdir(directory_path):
-        if filename.endswith(".json"):
+        if filename.endswith(".csv"):
             full_path = os.path.join(directory_path, filename)
-            with open(full_path, 'r') as f:
-                json_data = json.load(f)
-                df = pd.DataFrame([json_data])
-                df['bron_datum'] = filename
-                dataframes.append(df)
-    return pd.DataFrame(dataframes, ignore_index=True)
+            df = pd.read_csv(full_path)
+            df['bron_datum'] = filename[:-4]
+            dataframes.append(df)
+    
+    # Concatenate all DataFrames in the list into a single DataFrame
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    
+    return combined_df
+
+
+def to_percentage(number):
+    return str(np.round(number * 100, 2)) + "%"
+
+def compute_num_unique(df, n_births):
+    return len(np.unique(df['naam'].sample(n_births)))
+
+def plot_geboortedagen_per_jaar(bron, horizontale_lijn):
+    pl.figure(figsize=(20, 5))
+    bron.plot(kind="line", color="blue", title="Aantal geboortes per dag van het jaar", label='Aantal geboortes')
+    pl.axhline(horizontale_lijn, color='red', linestyle='dashed', linewidth=1, label='gemiddelde')
+    pl.xlabel("Dag van het jaar")
+    pl.ylabel("Aantal geboortes")
+    pl.grid(axis="y")
+    pl.legend()
+    pl.show()
+
+def plot_runnnig_average(df, horizontale_lijn):
+    pl.figure(figsize=(20, 5))
+    pl.plot(df['bron_datum'], df['running_average'], color="blue", label='Aantal geboortes')
+    pl.axhline(horizontale_lijn, color='red', linestyle='dashed', linewidth=1, label='gemiddelde')
+    pl.xlabel("Dag van het jaar")
+    pl.ylabel("Aantal geboortes")
+    pl.grid(axis="y")
+    pl.legend()
+    pl.show()
